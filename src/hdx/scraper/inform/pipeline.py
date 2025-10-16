2a7021348e5fcb7b72db2d196084fea77eb0a3c9
baripembo <erikawei@gmail.com>
2025-10-16 13:19:06 -0700

    init

---
+++ src/hdx/scraper/inform/pipeline.py
@@ -0,0 +1,131 @@
+#!/usr/bin/python
+"""Inform scraper"""
+
+import logging
+import pandas as pd
+from datetime import datetime
+from typing import List, Optional, Tuple
+
+from hdx.api.configuration import Configuration
+from hdx.data.dataset import Dataset
+from hdx.data.hdxobject import HDXError
+from hdx.location.country import Country
+from hdx.utilities.retriever import Retrieve
+from hdx.utilities.dateparse import default_date, default_enddate
+from slugify import slugify
+
+logger = logging.getLogger(__name__)
+
+
+class Pipeline:
+    def __init__(self, configuration: Configuration, retriever: Retrieve, tempdir: str):
+        self._configuration = configuration
+        self._retriever = retriever
+        self._tempdir = tempdir
+
+
+    def get_data(self, url_id, year_col) -> dict:
+        data_url = self._configuration[url_id]
+        data = self._retriever.download_json(data_url)
+
+        df = pd.DataFrame(data)
+        df = df[df[year_col] != 0].dropna(subset=[year_col])
+        df["CountryName"] = df["Iso3"].apply(lambda iso3: Country.get_country_name_from_iso3(iso3))
+        df_sorted = df.sort_values(by=["Iso3", year_col], ascending=[True, False])
+
+        if url_id == "latest_url":
+            new_order = ["CountryName", "Iso3", "ValidityYear", "IndicatorName", "IndicatorScore", "Unit"]
+            df_sorted = df_sorted[new_order]
+        else:
+            new_order = ["CountryName", "Iso3", "GNAYear", "IndicatorId", "FullName", "IndicatorScore", "WorkflowId", "MethodologyId"]
+            df_sorted = df_sorted[new_order]
+
+        # group_by_country = {iso: group.to_dict(orient="records") for iso, group in df_sorted.groupby("Iso3")}
+
+        return df_sorted
+
+    def generate_dataset(self, latest_df, trends_df) -> Optional[Dataset]:
+        dataset_title = "INFORM Risk Index"
+        dataset_name = slugify(dataset_title)
+
+        # Get date range
+        min_date, max_date = self.get_date_range(latest_df, trends_df)
+
+        # Dataset info
+        dataset = Dataset(
+            {
+                "name": dataset_name,
+                "title": dataset_title,
+            }
+        )
+
+        dataset.set_time_period(min_date, max_date)
+        dataset.add_tags(self._configuration["tags"])
+        dataset.add_other_location("world")
+        # Only if needed
+        # dataset.set_subnational(True)
+        # try:
+        #     dataset.add_country_location(dataset_country_iso3)
+        # except HDXError:
+        #     logger.error(f"Couldn't find country {dataset_country_iso3}, skipping")
+        #     return
+
+        # Latest resource
+        latest_resource_name = f"{slugify(self._configuration["latest_name"])}.csv"
+        latest_resource_data = {
+            "name": latest_resource_name,
+            "description": self._configuration["latest_description"],
+        }
+
+        dataset.generate_resource_from_iterable(
+            headers=list(latest_df.columns),
+            iterable=latest_df.to_dict(orient="records"),
+            hxltags={},
+            folder=self._tempdir,
+            filename=latest_resource_name,
+            resourcedata=latest_resource_data,
+            quickcharts=None,
+        )
+
+        # Trends resource
+        trends_resource_name = f"{slugify(self._configuration["trends_name"])}.csv"
+        trends_resource_data = {
+            "name": trends_resource_name,
+            "description": self._configuration["trends_description"],
+        }
+
+        dataset.generate_resource_from_iterable(
+            headers=list(trends_df.columns),
+            iterable=trends_df.to_dict(orient="records"),
+            hxltags={},
+            folder=self._tempdir,
+            filename=trends_resource_name,
+            resourcedata=trends_resource_data,
+            quickcharts=None,
+        )
+
+        return dataset
+
+    def get_date_range(self, latest_df, trends_df) -> Tuple:
+        """
+        Returns min and max date
+        """
+        latest_years = pd.to_numeric(latest_df["ValidityYear"], errors="coerce")
+        trends_years = pd.to_numeric(trends_df["GNAYear"], errors="coerce")
+
+        # Combine all valid years into one series
+        all_years = (
+            pd.concat([latest_years, trends_years])
+            .apply(pd.to_numeric, errors="coerce")
+            .dropna()
+            .astype(int)
+        )
+
+        # Make sure years are within valid range
+        this_year = datetime.now().year
+        all_years = all_years[(all_years >= 1900) & (all_years <= this_year)]
+
+        # Return the overall min and max dates
+        min_date = pd.to_datetime(f"{all_years.min()}-01-01")
+        max_date = pd.to_datetime(f"{all_years.max()}-01-01")
+        return min_date, max_date
---
+++ tests/test_pipeline.py
@@ -0,0 +1,88 @@
+from os.path import join
+
+from hdx.utilities.downloader import Download
+from hdx.utilities.path import temp_dir
+from hdx.utilities.retriever import Retrieve
+
+from hdx.scraper.inform.pipeline import Pipeline
+
+
+from os.path import join
+
+from hdx.utilities.compare import assert_files_same
+from hdx.utilities.downloader import Download
+from hdx.utilities.path import temp_dir
+from hdx.utilities.retriever import Retrieve
+
+from hdx.scraper.inform.pipeline import Pipeline
+
+
+class TestPipeline:
+    def test_pipeline(self, configuration, fixtures_dir, input_dir, config_dir):
+        with temp_dir(
+            "TestInform",
+            delete_on_success=True,
+            delete_on_failure=False,
+        ) as tempdir:
+            with Download(user_agent="test") as downloader:
+                retriever = Retrieve(
+                    downloader=downloader,
+                    fallback_dir=tempdir,
+                    saved_dir=input_dir,
+                    temp_dir=tempdir,
+                    save=False,
+                    use_saved=True,
+                )
+                pipeline = Pipeline(configuration, retriever, tempdir)
+
+                latest_data = pipeline.get_data("latest_url", "ValidityYear")
+                trends_data = pipeline.get_data("trends_url", "GNAYear")
+
+                dataset = pipeline.generate_dataset(latest_data, trends_data)
+                dataset.update_from_yaml(
+                    path=join(config_dir, "hdx_dataset_static.yaml")
+                )
+
+                assert dataset == {
+                    "caveats": None,
+                    "name": "inform-risk-index",
+                    "title": "INFORM Risk Index",
+                    "dataset_date": "[2011-01-01T00:00:00 TO 2025-01-01T23:59:59]",
+                    "tags": [
+                        {
+                            "name": "hazards and risk",
+                            "vocabulary_id": "b891512e-9516-4bf5-962a-7a289772a2a1",
+                        },
+                    ],
+                    "license_id": "cc-by",
+                    "methodology": "Composite Indicator",
+                    "dataset_source": "INFORM",
+                    "groups": [{"name": "world"}],
+                    "package_creator": "HDX Data Systems Team",
+                    "private": False,
+                    "maintainer": "fdbb8e79-f020-4039-ab3a-9adb482273b8",
+                    "owner_org": "e116c55a-d536-4b47-9308-94b1c7457afe",
+                    "data_update_frequency": 180,
+                    "notes": "The INFORM Risk Index is a global, open-source risk "
+                    "assessment for humanitarian crises and disasters. It can support "
+                    "decisions about prevention, preparedness and response."
+                }
+
+                resources = dataset.get_resources()
+                assert resources == [
+                    {
+                        "name": "inform-risk-index.csv",
+                        "description": "CSV containing the latest national INFORM Risk Index values.",
+                        "format": "csv",
+                    },
+                    {
+                        "name": "inform-risk-index-trends.csv",
+                        "description": "CSV containing national INFORM Risk Index trend data from the past 10 years.",
+                        "format": "csv",
+                    }
+                ]
+                for resource in resources:
+                    filename = resource["name"]
+                    actual = join(tempdir, filename)
+                    expected = join(fixtures_dir, filename)
+                    assert_files_same(actual, expected)
